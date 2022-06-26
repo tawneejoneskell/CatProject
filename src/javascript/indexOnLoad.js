3226d3e70e5ecb9721584654abd01d4c16bb97f1
tawneejoneskell <natawneetjk@gmail.com>
2022-06-26 11:05:37 -0700

    Css Animations

---
+++ src/javascript/indexOnLoad.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    console.log("this is being called")
+    secretMessage("Finch")
+}
+
+function secretMessage(message) {
+   console.log("Was " + message + " your secret message?");
+
+}
