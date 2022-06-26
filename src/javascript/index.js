d827134041d65697a22ca4a021581740e78efae7
Taylor Jones <taylor.jones@revature.net>
2022-06-26 14:44:34 -0400

    finished the cat search API and rendered image from provided URL.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,17 +1,43 @@
+let dropdown = document.getElementById("dropdownMenu");
+
+window.prepareString = function(catString) {
+    console.log('CALLING PREPARE STRING.')
+    return catString.replace(" ", "%20")
+}
+
+let dropDownValue = prepareString(dropdown.value);
+
+
+window.getChange = function(event){
+    dropDownValue = event.target.value;
+    dropDownValue = prepareString(dropDownValue);
+}
+
+dropdown.addEventListener("change", getChange);
+
+
 window.getCatData = async function(e){
     e.preventDefault();
-    var accessKey = `${process.env.TEST_KEY}`;
-    console.log(accessKey);
-
+    var accessKey = `${process.env.CAT_API_KEY}`;
     try{
-        let request= await fetch(`${process.env.CAT_API_ALL_BREEDS_URL}`, {
+        let request= await fetch(`${process.env.CAT_API_SEARCH_URL}${dropDownValue}`, {
             method: "GET",
             headers: {
                 'x-api-key': accessKey
             },
         });
         let result= await request.json();
-        let pictureURL= result[15].image.url;
+        console.log(result);
+        let breedId = result[0].id;
+
+        let imageRequest = await fetch(`${process.env.CAT_API_IMAGE_SEARCH_URL}${breedId}`, {
+            method: "GET",
+            headers: {
+                'x-api-key': accessKey
+            },
+        });
+        let imageResult = await imageRequest.json();
+        let pictureURL= imageResult[0].url;
         var newNode= document.createElement("img");
         newNode.src=pictureURL;
         document.getElementById("imageContainer").appendChild(newNode);
@@ -21,3 +47,4 @@ window.getCatData = async function(e){
         console.log(error);
     }
 }
+
