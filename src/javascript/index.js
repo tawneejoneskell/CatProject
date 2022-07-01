8216f1e0c9764b19c48f187713d398cfc2923951
Taylor Jones <taylor.jones@revature.net>
2022-07-01 17:26:23 -0400

    updated styling for almost every element.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,5 @@
 let dropdown = document.getElementById("dropdownMenu");
-let nameInput = document.getElementById("nameInput");
+let nameInputValue = nameInput.value;
 let reloadFlag = false;
 
 window.prepareString = function(catString) {
@@ -15,12 +15,18 @@ window.getChange = function(event){
 }
 
 window.getNameChange = function(event){
-    nameInput = event.target.value;
-    console.log(nameInput);
+    nameInputValue = event.target.value;
 }
 
 dropdown.addEventListener("change", getChange);
 nameInput.addEventListener("change", getNameChange);
+
+class NothingSelectedError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "Nothing Selected Error";
+    }
+}
 
 window.getCatData = async function(e){
     e.preventDefault();
@@ -41,21 +47,23 @@ window.getCatData = async function(e){
 
     var accessKey = `${process.env.CAT_API_KEY}`;
     try{
+        if(!dropDownValue){
+            throw new NothingSelectedError('There is nothing selected in the cat menu.');
+        }
         let request= await fetch(`${process.env.CAT_API_SEARCH_URL}${dropDownValue}`, {
             method: "GET",
             headers: {
                 'x-api-key': accessKey
-            },
+            }, 
         });
         // this is the RESULT of the API call.
         let result= await request.json();
-        // it's being console logged for you here so you can read it in your browser.
         console.log(result);
+        // it's being console logged for you here so you can read it in your browser.
         let breedId = result[0].id;
         let intelligence = result[0].intelligence;
         let shedding_level = result[0].shedding_level;
         let social_needs = result[0].social_needs;
-        console.log(social_needs)
 
         let imageRequest = await fetch(`${process.env.CAT_API_IMAGE_SEARCH_URL}${breedId}`, {
             method: "GET",
@@ -81,11 +89,13 @@ window.getCatData = async function(e){
         newNode.style.maxHeight = "350px";
         newNode.style.maxWidth = "350px";
         newNode.style.marginTop = '20px';
-        newNode.style.borderRadius = '4px';
         newNode.style.boxShadow = '.5px 1px 15px  #9400d3';
         newNode.setAttribute('id', 'catImage')
         //
         imageContainer.replaceChild(newNode, imageContainer.childNodes[0]);
+        // update hyperlink
+        const wikiLink = document.getElementById("wikiLink");
+        wikiLink.setAttribute("href", result[0].wikipedia_url);
 
         const intelligenceCell = document.getElementById("intelligenceCell");
         intelligenceCell.innerHTML = "<p>" + intelligence + "</p>";
@@ -93,10 +103,12 @@ window.getCatData = async function(e){
         sheddingCell.innerHTML= "<p>" + shedding_level + "</p>";
         const socialCell = document.getElementById("socialCell");
         socialCell.innerHTML = "<p>" + social_needs + "</p>";
+        const tableContainer = document.getElementById("tableContainer");
+        tableContainer.style.display = "block";
 
         
-    }catch(error){
-        console.log(error);
+    }catch(NothingSelectedError){
+        console.log(NothingSelectedError);
 
         const errorIcon = document.createElement('img');
         errorIcon.style.maxHeight = '150px';
@@ -109,6 +121,10 @@ window.getCatData = async function(e){
 
 window.onNameChange = function(){
     const askExperts = document.getElementById("askExperts");
-    askExperts.innerHTML = "Hello " + nameInput + ", is this the cat for you?";
+    if(nameInputValue.length > 0){
+    askExperts.innerHTML = "Hello " + nameInputValue + ", is this the cat for you? (Wikipedia.com)";
+    } else {
+    askExperts.innerHTML = "Ask the Experts: "
+    }
 }
 
